fdea3d49898417adc56f2ca606f61314299cf686
Thomas <thomasreichmann@hotmail.com>
2021-10-30 23:56:31 -0300

    home filled with info

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,14 +1,26 @@
+import ExternalLink from '../generics/externalLink/ExternalLink';
+
 export default function Home() {
 	let age = new Date().getFullYear() - 2003;
 
 	return (
 		<main>
 			<h1>e ai</h1>
-			<p>
+			<p style={{ lineHeight: 1.85 }}>
 				{age} anos 👦
 				<br />
 				full stack developer
 				<br />
+				trabalhando de São Paulo na <ExternalLink to="https://unike.tech/">unike.TECH</ExternalLink>
+				<br />
+			</p>
+			<br />
+			<p>email: thomasalmeidar@gmail.com</p>
+			<p>
+				github: <ExternalLink to="https://github.com/thomasreichmann">@thomasreichmann</ExternalLink>
+			</p>
+			<p>
+				linkedin: <ExternalLink to="https://linkedin.com/in/thomasreichmann">@thomasreichmann</ExternalLink>
 			</p>
 		</main>
 	);
