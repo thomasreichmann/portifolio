d94dc70df969f9ae4aa70fff6ffec8fcbe14ce7d
Thomas <thomasreichmann@hotmail.com>
2022-04-16 18:20:10 -0300

    added the language button to header without content

--- src/generics/header/Header.tsx
+++ src/generics/header/Header.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MultiSelectButton from '../multiSelectButton/MultiSelectButton';
 import NavBar from '../navBar/NavBar';
 import './Header.scss';
 
 export default function Header() {
 	return (
 		<header className="Header App-header">
-			<h2>
-				<Link to="/">thomas reichmann</Link>
-			</h2>
+			<div className="top-header">
+				<h2>
+					<Link to="/">thomas reichmann</Link>
+				</h2>
+				<MultiSelectButton />
+			</div>
 			<NavBar />
 		</header>
 	);
