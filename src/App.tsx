cb5e73384cb97b1621d48506122cef45bdb9b5d1
Thomas <thomasreichmann@hotmail.com>
2022-04-25 01:02:25 -0300

    first full implementation of the language button

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './home/Home';
 import Projects from './projects/Projects';
-import NavBar from './generics/navBar/NavBar';
 import { IntlProvider } from 'react-intl';
 
 import en from './translation/en.json';
 import pt from './translation/pt.json';
+
 import Header from './generics/header/Header';
 
-type Locale = 'pt' | 'en';
+export const LOCALE_LIST = ['en', 'pt'] as const;
+export type Locale = typeof LOCALE_LIST[number];
 
 function App() {
 	const [locale, setLocale] = React.useState<Locale>('en');
 
 	const messages = { en, pt };
 
+	const handleLocaleChange = (newLocale: Locale) => {
+		setLocale(newLocale);
+	};
+
 	return (
 		<IntlProvider locale={locale} messages={messages[locale]}>
 			<Router>
 				<div className="App">
 					{/* Header with title and nav buttons */}
-					<Header />
+					<Header onLocaleChanged={handleLocaleChange} />
 					{/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
 					<Switch>
--- src/generics/header/Header.tsx
+++ src/generics/header/Header.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Locale, LOCALE_LIST } from '../../App';
 import MultiSelectButton from '../multiSelectButton/MultiSelectButton';
 import NavBar from '../navBar/NavBar';
 import './Header.scss';
 
-export default function Header() {
+interface HeaderProps {
+	onLocaleChanged: (locale: Locale) => void;
+}
+
+export default function Header(props: HeaderProps) {
 	return (
 		<header className="Header App-header">
 			<div className="top-header">
 				<h2>
 					<Link to="/">thomas reichmann</Link>
 				</h2>
-				<MultiSelectButton />
+				<div className="language-select">
+					<MultiSelectButton
+						values={LOCALE_LIST as unknown as string[]}
+						onValueChanged={value => props.onLocaleChanged(value as Locale)}
+					/>
+				</div>
 			</div>
 			<NavBar />
 		</header>
