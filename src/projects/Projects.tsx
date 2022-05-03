c7d5c7ca7577e75e99356b5b82de63b2c0e0bd64
Thomas <thomasreichmann@hotmail.com>
2022-05-03 00:54:20 -0300

    added styling and temp content to projects page

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,8 +1,37 @@
+import { FormattedMessage } from 'react-intl';
+import './Projects.scss';
+
 export default function Projects() {
 	return (
-		<div>
-			<h2>Projects</h2>
-			<h1>Projects</h1>
-		</div>
+		<main className="Projects">
+			<h1>
+				<FormattedMessage id="PROJECTS_HEADER" />
+			</h1>
+			<p>
+				<FormattedMessage id="PROJECTS_TITLE" />
+			</p>
+			<ol reversed>
+				<li>
+					<h3>
+						<a href="https://github.com/thomasreichmann/portifolio" className="listLink">
+							portifolio
+						</a>
+					</h3>
+					<p>
+						<FormattedMessage id="PROJECTS_PORTIFOLIO_CONTENT" />
+					</p>
+				</li>
+				<li>
+					<h3>
+						<a href="https://github.com/thomasreichmann/discord-bot-template" className="listLink">
+							discord bot template
+						</a>
+					</h3>
+					<p>
+						<FormattedMessage id="PROJECTS_BOT_TEMPLATE_CONTENT" />
+					</p>
+				</li>
+			</ol>
+		</main>
 	);
 }
